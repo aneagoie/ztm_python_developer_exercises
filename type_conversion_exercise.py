b4985e2bc67b4bbbb5498d6b398f74883ac3a6e4
jondmulcahy <126435108+jondmulcahy@users.noreply.github.com>
2023-10-06 21:09:22 -0700

    Add files via upload

---
+++ type_conversion_exercise.py
@@ -0,0 +1,6 @@
+# exercise - enter your age given the intial statement on line 3
+
+birth_year = input('What year were you born? ')
+age = 2023 - int(birth_year)
+
+print(f'You are {age} years old!')
