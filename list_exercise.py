1170d7c6648045bd8a8122ad6b1c5af372e8102a
jondmulcahy <126435108+jondmulcahy@users.noreply.github.com>
2023-10-08 08:51:42 -0700

    Add files via upload

---
+++ list_exercise.py
@@ -0,0 +1,9 @@
+# fix this code so that it prints a sorted list of all of our friends (alphabetical). 
+friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
+
+new_friend = ['Stanley']
+
+# print(friends.sort() + new_friend)
+
+friends.sort()
+print(friends + new_friend)
