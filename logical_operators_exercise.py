4953a12ff894cf335a558a54c1d90e8019cdd9b3
jondmulcahy <126435108+jondmulcahy@users.noreply.github.com>
2023-10-09 09:34:55 -0700

    Add files via upload

---
+++ logical_operators_exercise.py
@@ -0,0 +1,15 @@
+is_magician = False
+is_expert = True
+
+# check if magician AND expert: "You're a master magician!"
+
+# check if magician BUT not an expert: "At least you're getting there."
+
+# If you're not a magician "You need magic powers."
+
+if is_magician and is_expert:
+    print('You\'re a master magician!')
+elif is_magician and not is_expert:
+    print('At least you\'re getting there.')
+elif not is_magician:
+    print('You need magic powers!')
