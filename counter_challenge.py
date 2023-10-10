bc2e94b9e730ad6994c2b30fa176c98275519d8b
jondmulcahy <126435108+jondmulcahy@users.noreply.github.com>
2023-10-10 00:40:34 -0700

    Add files via upload

---
+++ counter_challenge.py
@@ -0,0 +1,9 @@
+# Create a counter to loop over the iterable and find the sum total of the following list
+
+my_list = [1,2,3,4,5,6,7,8,9,10]
+counter = 0
+
+for item in my_list:
+    counter = counter + item
+
+print(counter)
