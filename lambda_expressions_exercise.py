fa70ca2fc2ce2fc834d4f77eec409c506f4ec481
JD Mulcahy <126435108+jondmulcahy@users.noreply.github.com>
2023-11-23 09:11:12 -0700

    Add files via upload

---
+++ lambda_expressions_exercise.py
@@ -0,0 +1,10 @@
+#Create a lambda expression to square the following:
+my_list = [5,4,3]
+
+print(list(map(lambda num: num**2, my_list)))
+
+#Sort tuple by the second key
+a = [(0,2), (4,3), (9,9), (10,-1)]
+
+a.sort(key=lambda x: x[1])
+print(a)
